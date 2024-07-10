4b0471fb44fd42ac48a2af090d6943067121960e
Guesly Saint-Aubin <GSaintAubin@macbook-pro.mynetworksettings.com>
2024-07-10 00:54:03 -0400

    changes to card color and content

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,14 @@
 import React from "react";
 
-const Card = ({ name, email, id }) => {
+// Card Component
+
+const Card = ({ name, email, id, username }) => {
     return (
-        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+        <div className='tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
             <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
             <>
                 <h2>{name}</h2>
+                <h3>'{username}'</h3>
                 <p>{email}</p>
             </>
         </div>
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,15 +1,18 @@
 import React from "react";
 import Card from "./Card";
 
+// Filtering Component
+
 const CardList = ({ robots }) => {
     return (
         <>
-            {robots.map((user, i, id) => {
+            {robots.map((user, i, id, username) => {
                 return (
                     <Card
                         key={i}
                         id={robots[i].id}
                         name={robots[i].name}
+                        username={robots[i].username}
                         email={robots[i].email}
                     />
                 )
